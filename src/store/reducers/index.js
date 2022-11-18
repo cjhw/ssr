24a98ceedc20ccf7b205bf19681118390bc0906e
caijianhaoa <1784581164@qq.com>
2022-11-18 15:20:18 +0800

    delete

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import homeReducer from './home';
-import personalReducer from './personal';
-
-export default combineReducers({
-  home: homeReducer,
-  personal: personalReducer,
-});
